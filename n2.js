87914946008be44981dd0f931c5900eb1eab087a
aneeshsp <118538514+aneeshsp@users.noreply.github.com>
2023-08-23 00:12:47 +0900

    Update n2.js

--- n2.js
+++ n2.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburgerIcon = document.querySelector(".hamburger-icon");
+    const navList = document.querySelector(".nav-container ul");
+
+    hamburgerIcon.addEventListener("click", function() {
+        navList.classList.toggle("show");
+    });
+});
 
