7f82988ced27009725ef8e5a1c69f7e2bd8d9b60
Aneesh Patil <aneesh@duck.com>
2023-08-28 02:13:37 +0900

    js

--- n2.js
+++ n2.js
@@ -7,3 +7,16 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+// Define a function to toggle the display of the dropdown content
+function toggleDropdown(menu) {
+    // Get the dropdown content element by its id
+    var dropdown = document.getElementById(menu);
+    // Check if the dropdown content is hidden or shown
+    if (dropdown.style.display === "none") {
+        // If hidden, show it
+        dropdown.style.display = "block";
+    } else {
+        // If shown, hide it
+        dropdown.style.display = "none";
+    }
+}
